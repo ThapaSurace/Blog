b9c6e6216f18784b52e635c29349d75923b22dd7
Suresh Thapa <zorosenpai456@gmail.com>
2024-01-08 18:45:31 +0545

    commit

--- app/utils/fetchApi.js
+++ app/utils/fetchApi.js
@@ -1,7 +1,7 @@
 import { BASE_URL } from "@/libs/BaseUrl";
 
   export const getData = async () => {
-    const res = await fetch(`${BASE_URL}/post`,{cache:"no-store"});
+    const res = await fetch("http://localhost:3000/api/post",{cache:"no-store"});
     if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Failed to fetch data')
@@ -10,7 +10,7 @@ import { BASE_URL } from "@/libs/BaseUrl";
   };
 
   export const getSinglePost = async (id) => {
-    const res =  await fetch(`${BASE_URL}/post/${id}`)
+    const res =  await fetch("http://localhost:3000/api/post/${id}")
     if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Failed to fetch data')
